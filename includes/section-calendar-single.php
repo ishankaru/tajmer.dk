b0e610353d1221262a9af1f7bc7e2fee748d08b7
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-17 14:55:59 +0100

    Theme Commit

--- includes/section-calendar-single.php
+++ includes/section-calendar-single.php
@@ -16,19 +16,33 @@
 						<h2>Kalender</h2>
 					</div>
 					<div class="col-md-12">
-						<table class="table table-striped table-hover">
-							<tbody>
-								<?php while ( have_rows('artist_calendar') ) : the_row(); ?>
-						    	<tr>
-									<td><img src="<?php the_field('artist_calendar_image'); ?>" alt="<?php the_sub_field('title'); ?>"></td>
-									<td><?php the_sub_field('date'); ?></td>
-									<td><?php the_sub_field('title'); ?></td>
-									<td><?php the_sub_field('location'); ?>, <?php the_sub_field('city'); ?></td>
-									<td><a href="<?php the_sub_field('url'); ?>" title="<?php the_sub_field('title'); ?>" class="btn btn-block btn-success"><?php the_sub_field('button_text'); ?></a></td>
-								</tr>
-							<?php endwhile; ?>
-							</tbody>
-						</table>
+						<div class="table-responsive container-calendar">
+							<table class="table table-striped table-hover">
+								<tbody>
+									<?php while ( have_rows('artist_calendar') ) : the_row(); ?>
+							    	<tr>
+										<td>
+											<?php if( get_field('artist_calendar_image') ): ?>
+												<img src="<?php the_field('artist_calendar_image'); ?>" alt="<?php the_sub_field('title'); ?>">
+											<?php endif; ?>
+										</td>
+										<td>
+											<?php the_sub_field('date'); ?>
+										</td>
+										<td>
+											<?php the_sub_field('title'); ?>
+										</td>
+										<td>
+											<?php the_sub_field('location'); ?>, <?php the_sub_field('city'); ?>
+										</td>
+										<td>
+											<a href="<?php the_sub_field('url'); ?>" title="<?php the_sub_field('title'); ?>" class="btn btn-block btn-success"><?php the_sub_field('button_text'); ?></a>
+										</td>
+									</tr>
+								<?php endwhile; ?>
+								</tbody>
+							</table>
+						</div>
 					</div>
 					<div class="col-md-12">
 						<a href="#" class="lead more">Se den fulde kalender her</a>
