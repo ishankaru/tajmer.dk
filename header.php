957ec21d03c6f5199b37bdab7d3136caafd0ad71
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-17 09:26:14 +0100

    Added Google Analytics

--- header.php
+++ header.php
@@ -13,13 +13,27 @@
 
 	<?php wp_head(); ?>
 
+	<?php include 'includes/styles.php';?>
+
+	<link rel="icon" href="http://www.tajmer.dk/favicon.ico" type="image/x-icon" />
+	
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-12693954-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
 	<!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-
-	<?php include 'includes/styles.php';?>
-	<link rel="icon" href="http://www.tajmer.dk/favicon.ico" type="image/x-icon" />
 	
 </head>
+
 <body class="nav-is-fixed">
