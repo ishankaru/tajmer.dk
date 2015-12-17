0f9142f405a38bce828533b12f11d63b8d278ed6
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-17 16:00:09 +0100

    Theme Commit

--- includes/section-related.php
+++ includes/section-related.php
@@ -23,7 +23,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<div class="gallery gallery-main js-flickity" data-flickity-options='{ "contain": true, "lazyLoad": 6, "pageDots": false, "wrapAround": true, "prevNextButtons": true}'>
+				<div class="gallery gallery-main js-flickity" data-flickity-options='{ "contain": true, "lazyLoad": 6, "pageDots": true, "wrapAround": true, "prevNextButtons": true}'>
 					<?php $orig_post = $post; global $post; if ( 'artists' == get_post_type() ) {
 						$tags = wp_get_post_terms($post->ID, 'type');
 						if ($tags) { $tag_ids = array();
