85874ac80e6b876660b04d64fe3d4bf0b376ecd9
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-18 23:42:13 +0100

    Theme commit

--- includes/section-header.php
+++ includes/section-header.php
@@ -27,20 +27,20 @@
 	</ul> 
 </header>
 
-<main class="cd-main-content">
-
- 	<section class="module module-subnav hidden-xs hidden-sm">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4 col-lg-5">
-					<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-						<label><i class="fa fa-search"></i></label>
-						<input type="search" placeholder="Søg artist..." value="" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-					</form>
-				</div>
-				<div class="col-md-8 col-lg-7 text-right">
-					<span>Ring til os eller <a href="<?php echo get_home_url(); ?>/forespoergsel/" title="#">send en forespørgsel</a> på en af vores artister <a href="#" title="#"> <i class="fa fa-phone"></i> +45 46 15 37 00</a></span>
-				</div>
+<section class="module module-subnav hidden-xs hidden-sm">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-lg-5">
+				<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+					<label><i class="fa fa-search"></i></label>
+					<input type="search" placeholder="Søg artist..." value="" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+				</form>
+			</div>
+			<div class="col-md-8 col-lg-7 text-right">
+				<span>Ring til os eller <a href="<?php echo get_home_url(); ?>/forespoergsel/" title="#">send en forespørgsel</a> på en af vores artister <a href="#" title="#"> <i class="fa fa-phone"></i> +45 46 15 37 00</a></span>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
+
+<main class="cd-main-content">
