0be38bfd00e89edc48b0cbce569b9152fdb3c714
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-18 14:30:11 +0100

    Theme Commit

--- includes/section-calendar.php
+++ includes/section-calendar.php
@@ -18,36 +18,37 @@
 	    'order' => 'ASC'
 	));
 ?>
-
-<div class="col-md-12">
-	<div class="table-responsive container-calendar">
-		<table class="table table-striped table-hover">
-			<tbody>
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php while ( have_rows('artist_calendar') ) : the_row(); ?>
-			    	<tr>
-						<td>
-							<?php if( get_field('artist_calendar_image') ): ?>
-								<img src="<?php the_field('artist_calendar_image'); ?>" alt="<?php the_sub_field('title'); ?>">
-							<?php endif; ?>
-						</td>
-						<td>
-							<?php the_sub_field('date'); ?>
-						</td>
-						<td>
-							<?php the_sub_field('title'); ?>
-						</td>
-						<td>
-							<?php the_sub_field('location'); ?>, <?php the_sub_field('city'); ?>
-						</td>
-						<td>
-							<a href="<?php the_sub_field('url'); ?>" title="<?php the_sub_field('title'); ?>" class="btn btn-block btn-success"><?php the_sub_field('button_text'); ?></a>
-						</td>
-					</tr>
-					<?php endwhile; ?>
-				<?php endwhile; endif; ?>
-			</tbody>
-		</table>
+<div class="row">
+	<div class="col-md-12">
+		<div class="table-responsive">
+			<table class="table table-striped table-hover">
+				<tbody>
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<?php while ( have_rows('artist_calendar') ) : the_row(); ?>
+				    	<tr>
+							<td>
+								<?php if( get_field('artist_calendar_image') ): ?>
+									<img src="<?php the_field('artist_calendar_image'); ?>" alt="<?php the_sub_field('title'); ?>">
+								<?php endif; ?>
+							</td>
+							<td>
+								<?php the_sub_field('date'); ?>
+							</td>
+							<td>
+								<?php the_sub_field('title'); ?>
+							</td>
+							<td>
+								<?php the_sub_field('location'); ?>, <?php the_sub_field('city'); ?>
+							</td>
+							<td>
+								<a href="<?php the_sub_field('url'); ?>" title="<?php the_sub_field('title'); ?>" class="btn btn-block btn-success"><?php the_sub_field('button_text'); ?></a>
+							</td>
+						</tr>
+						<?php endwhile; ?>
+					<?php endwhile; endif; ?>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </div>
 <?php wp_reset_query(); ?>
