0cffb5ee8f96ef91f7e39536512442429003b868
vick2015 <vb.net20010@gmail.com>
2015-12-18 22:20:54 +0530

    Update page-calendar.php

--- page-calendar.php
+++ page-calendar.php
@@ -10,84 +10,17 @@
  get_header(); ?>
 
  <?php include 'includes/section-header.php';?>
-
-	<section class="module module-calendar">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-			<?php	$today = date('Ymd');
 $args = array(
     'post_type' => 'artists',
-    'post_status' => 'publish',
-    'posts_per_page' => '-1',
-    );
+    'posts_per_page' => 5
+);
 
-$query = new WP_Query( $args ); ?>
+$query = new WP_Query( $args );
+if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
+    the_title(); // whatever you want to use from $post
+endwhile; endif;
 
-<?php var_dump($query->the_post()); ?>
-
-					<div class="row">
-						<div class="col-md-12">
-
-							<h1>Comedy Calender</h1>
-							<p>Text goes here</p>
-							<hr>
-							<div class="table-responsive">
-								<table class="table table-striped table-hover">
-									<tbody>
-										<?php if ($query->have_posts()) : while ($query->have_posts()) : the_post(); ?>
-											<?php while ( $query->have_rows('artist_calendar') ) : $query->the_row(); ?>
-
-											
-
-									    	<tr>
-												<td>
-													<?php if( get_field('artist_calendar_image') ): ?>
-														<img src="<?php the_field('artist_calendar_image'); ?>" alt="<?php the_sub_field('title'); ?>">
-													<?php endif; ?>
-												</td>
-												<td>
-													<?php 
-														$dateformatstring = "d. F Y";
-														$unixtimestamp = strtotime(get_sub_field('date'));
-														echo 	$unixtimestamp;
-														echo date_i18n($dateformatstring, $unixtimestamp);
-													?>
-
-														<?php $eventdate = new DateTime(get_sub_field('date'));
-												$now = new DateTime();
-
-												if($eventdate < $now) {
-												    echo 'date is in the past';
-												}
-
-												if($eventdate > $now) {
-												    echo 'date is in the future';
-												} ?>
-												</td>
-												<td>
-													<?php the_sub_field('title'); edit_post_link(' - Rediger event', '', ''); ?>
-												</td>
-												<td>
-													<?php the_sub_field('location'); ?>, <?php the_sub_field('city'); ?>
-												</td>
-												<td>
-													<a href="<?php the_sub_field('url'); ?>" title="<?php the_sub_field('title'); ?>" class="btn btn-block btn-success"><?php the_sub_field('button_text'); ?></a>
-												</td>
-											</tr>
-											<?php endwhile; ?>
-										<?php endwhile; endif; ?>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>						
-				</div>
-			</div>
-		</div>
-	</section>				
-
-	<?php include 'includes/section-footer.php';?>
+<?php include 'includes/section-footer.php';?>
 
 <?php get_footer(); ?>
 
