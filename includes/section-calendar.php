7ff98b3d7a9cf551d6ea4431991eeadd3dcd3342
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-18 23:17:05 +0100

    Theme Commit

--- includes/section-calendar.php
+++ includes/section-calendar.php
@@ -9,50 +9,66 @@
  */
 ?>
 
+	<?php 	
 
-<?php // Get all posts from Custom Post Type, sort by attribute
-	query_posts(array(
-	    'post_type' => 'artists',
-	    'posts_per_page' => -1,
-	    'orderby' => 'menu_order',
-	    'order' => 'ASC'
-	));
-?>
-<div class="row">
-	<div class="col-md-12">
-		<div class="table-responsive">
-			<table class="table table-striped table-hover">
-				<tbody>
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<?php while ( have_rows('artist_calendar') ) : the_row(); ?>
-				    	<tr>
-							<td>
-								<?php if( get_field('artist_calendar_image') ): ?>
-									<img src="<?php the_field('artist_calendar_image'); ?>" alt="<?php the_sub_field('title'); ?>">
-								<?php endif; ?>
-							</td>
-							<td>
-								<?php 
-									$dateformatstring = "d. F Y";
-									$unixtimestamp = strtotime(get_sub_field('date'));
-									echo date_i18n($dateformatstring, $unixtimestamp);
-								?>
-							</td>
-							<td>
-								<?php the_sub_field('title'); ?>
-							</td>
-							<td>
-								<?php the_sub_field('location'); ?>, <?php the_sub_field('city'); ?>
-							</td>
-							<td>
-								<a href="<?php the_sub_field('url'); ?>" title="<?php the_sub_field('title'); ?>" class="btn btn-block btn-success"><?php the_sub_field('button_text'); ?></a>
-							</td>
-						</tr>
-						<?php endwhile; ?>
-					<?php endwhile; endif; ?>
-				</tbody>
-			</table>
+		$calendar = array(); 
+
+		query_posts(array(
+		    'post_type' => 'artists',
+		    'posts_per_page' => -1
+		));
+
+		if (have_posts()) : 
+		 	while (have_posts()) : the_post(); 
+				while ( have_rows('artist_calendar') ) : the_row(); 
+					$eventdate = new DateTime(get_sub_field('date'));
+					$now = new DateTime();
+					if($eventdate > $now) {
+						$calendar[strtotime($eventdate)] = array(
+							get_sub_field('title'),
+							get_sub_field('location'),
+							get_sub_field('city'),
+							get_sub_field('url'),
+							get_sub_field('button_text'));
+					}
+				endwhile; 
+			endwhile; 
+		endif;
+	?>	
+
+	<div class="row">
+		<?php ksort($calendar); ?>								
+		<div class="col-md-12">
+			<div class="table-responsive">
+				<table class="table table-striped table-hover">
+					<tbody>
+						<?php foreach($calendar as $k => $v): ?>
+						    	<tr>
+								<td>
+									<img src="<?php the_field('artist_calendar_image'); ?>" alt="<?php echo $v[1] ?>">
+								</td>
+								<td>
+									<?php 
+										$dateformatstring = "d. F Y";
+										$unixtimestamp = $k;
+										echo date_i18n($dateformatstring, $unixtimestamp);
+									?>
+								</td>
+								<td>
+									<?php echo $v[1]; edit_post_link(' - Rediger event', '', ''); ?>
+								</td>
+								<td>
+									<?php echo $v[2] ?>, <?php echo $v[3]; ?>
+								</td>
+								<td>
+									<a href="<?php echo $v[4]; ?>" title="<?php  echo $v[1];; ?>" class="btn btn-block btn-success"><?php  echo $v[5]; ?></a>
+								</td>
+							</tr>
+						<?php endforeach;?>
+					</tbody>
+				</table>
+			</div>
 		</div>
-	</div>
-</div>
+	</div>	
+
 <?php wp_reset_query(); ?>
