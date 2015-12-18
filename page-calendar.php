6f07eaacfb2d82001a88bc10bd977757fcc22a83
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-18 19:29:26 +0100

    Update page-calendar.php

--- page-calendar.php
+++ page-calendar.php
@@ -15,8 +15,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-<?php $sortedArray = array(); ?>
-					<?php // Get all posts from Custom Post Type, sort by attribute
+					
+					<?php 	$sortedArray = array(); // Get all posts from Custom Post Type, sort by attribute
 						query_posts(array(
 						    'post_type' => 'artists',
 						    'posts_per_page' => -1,
@@ -24,20 +24,20 @@
 						    'order' => 'ASC'
 						));
 					?>
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-											<?php while ( have_rows('artist_calendar') ) : the_row(); ?>
-<?php $eventdate = new DateTime(get_sub_field('date'));
-												$now = new DateTime();
-												if($eventdate > $now) {
-												   	$sortedArray[strtotime(get_sub_field('date'))] = array(get_field('artist_calendar_image'),get_sub_field('title'),get_sub_field('location'),get_sub_field('city'),get_sub_field('url'),get_sub_field('button_text'));
-										}	?>
-		<?php endwhile; ?>
-										<?php endwhile;  endif; ?>	
+					
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<?php 	while ( have_rows('artist_calendar') ) : the_row(); 
+							$eventdate = new DateTime(get_sub_field('date'));
+							$now = new DateTime();
+							if($eventdate > $now) {
+								$sortedArray[strtotime(get_sub_field('date'))] = array(get_field('artist_calendar_image'),get_sub_field('title'),get_sub_field('location'),get_sub_field('city'),get_sub_field('url'),get_sub_field('button_text'));
+							}
+						endwhile; ?>
+					<?php endwhile;  endif; ?>	
 		
 					<div class="row">
-<?php ksort($sortedArray); ?>								
+						<?php ksort($sortedArray); ?>								
 						<div class="col-md-12">
-
 							<h1>Comedy Calender</h1>
 							<p>Text goes here</p>
 							<hr>
@@ -45,22 +45,16 @@
 								<table class="table table-striped table-hover">
 									<tbody>
 										<?php foreach($sortedArray as $k => $v): ?>
-
-									    	<tr>
+										    	<tr>
 												<td>
-													
-														<img src="<?php echo $v[0] ?>" alt="<?php echo $v[1] ?>">
-												
+													<img src="<?php echo $v[0] ?>" alt="<?php echo $v[1] ?>">
 												</td>
 												<td>
 													<?php 
 														$dateformatstring = "d. F Y";
 														$unixtimestamp = $k;
-													
 														echo date_i18n($dateformatstring, $unixtimestamp);
 													?>
-
-														
 												</td>
 												<td>
 													<?php echo $v[1]; edit_post_link(' - Rediger event', '', ''); ?>
@@ -75,8 +69,6 @@
 										<?php endforeach;?>
 									</tbody>
 								</table>
-								
-							
 							</div>
 						</div>
 					</div>						
