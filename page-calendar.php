4432076d69e8e3ceb4d9d7ba54f585e08e4ab82e
vick2015 <vb.net20010@gmail.com>
2015-12-18 22:23:08 +0530

    Update page-calendar.php

--- page-calendar.php
+++ page-calendar.php
@@ -12,12 +12,12 @@
  <?php include 'includes/section-header.php';?>
 <?php $args = array(
     'post_type' => 'artists',
-    'posts_per_page' => 5
+    'posts_per_page' => -1
 );
 
 $query = new WP_Query( $args );
 if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
-    the_title(); // whatever you want to use from $post
+    echo get_sub_field('date')// whatever you want to use from $post
 endwhile; endif;
 ?>
 <?php include 'includes/section-footer.php';?>
