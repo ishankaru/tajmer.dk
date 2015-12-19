d261b4b05997e31e54d4c4317bf73c139f69d3ba
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-19 12:44:48 +0100

    Theme Commit

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -11,11 +11,9 @@
 
 <?php if( get_field('hero_image') ): ?>
 	<section class="module module-hero <?php if( get_field('colorscheme') ) { ?>dark<?php } ?>" style="background-image:url('<?php the_field('hero_image');?>');">
-	 	<?php if ( is_singular() ): ?>
-	  		<figcaption>
-		  		<span class="artist"><?php the_title();?></span>
-		  		<span class="categories"><?php the_field('artist_subtitle');?></span>
-		  	</figcaption>
-		<?php endif; ?>
+	  	<figcaption>
+		  	<span class="artist"><?php the_title();?></span>
+		  	<span class="categories"><?php the_field('artist_subtitle');?></span>
+		</figcaption>
 	</section>	
 <?php endif; ?>
