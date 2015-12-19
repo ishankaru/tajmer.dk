efc74bfacf680102b2caf9053fa98436dc7e08d4
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-19 12:03:44 +0100

    Theme Commit

--- header.php
+++ header.php
@@ -9,17 +9,14 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	
-	<title><?php wp_title(); ?></title>
+	<title>
+		<?php wp_title(); ?>
+	</title>
 
 	<?php wp_head(); ?>
 
 	<link rel="icon" href="http://www.tajmer.dk/favicon.ico" type="image/x-icon" />
 
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-	
 </head>
 
 <body class="nav-is-fixed">
