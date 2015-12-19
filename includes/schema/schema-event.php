3b39c31b08e2cebe58217bad72b6e46ecd7bb7ab
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-19 15:37:17 +0100

    Theme Commit

--- includes/schema/schema-event.php
+++ includes/schema/schema-event.php
@@ -14,7 +14,7 @@
 
   [ 
 
-    <?php $events = array(); while ( have_rows('artist_calendar') ) : the_row(); ?>
+    <?php while ( have_rows('artist_calendar') ) : the_row(); ?>
       {
         "@context" : "http://schema.org",
         "@type" : "ComedyEvent",
@@ -46,8 +46,8 @@
           "priceCurrency": "DKK",
           "url" : "<?php the_sub_field('url'); ?>"
         }
-      }
-    <?php endwhile; echo implode(', ' , $events); ?>
+      },
+    <?php endwhile; ?>
   ]
 
 </script>
