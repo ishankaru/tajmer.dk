7d77072349c3f08974c0afc1ac395dbfea3b249d
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-21 14:42:00 +0100

    Update page-exclusive.php

--- page-exclusive.php
+++ page-exclusive.php
@@ -1,1 +1,58 @@
+<?php
+/**
+ * Template Name: Template: Exclusive Artists
+ *
+ * @package Tajmer Booking & Management
+ * @subpackage tajmer.dk
+ * @since Version 2.0
+ * @author Mikkel Tschentscher
+ */
+ get_header(); ?>
 
+ <?php include 'includes/section-header.php';?>
+
+  <section class="module module module-artists">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+
+          <h2>Eksklusive artister</h2>
+          
+          <?php 	
+						$posts = get_posts(array(
+							'numberposts'	=> -1,
+							'post_type'		=> 'artists',
+							'meta_query' => array(
+								array(
+									'key' => 'exclusive',
+									'value' => '1',
+									'compare' => '=='
+								)
+							)
+						))
+					?>
+							
+							
+          <div class="row">
+            <?php if( $posts ) { foreach( $posts as $post ) { setup_postdata( $post ); ?> 
+                <article class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+                  <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                  <?php if( get_field('artist_thumbnail') ): ?>
+                    <div class="thumbnail" style="background-image:url('<?php the_field('artist_thumbnail'); ?>')"></div>
+                  <?php else: ?>
+                    <div class="thumbnail" style="background-image:url('http://placehold.it/275x200');"></div>
+                  <?php endif; ?>
+                    <h2 class="h5"><?php the_title(); ?></h2>
+                  </a>
+                </article>
+                <?php } wp_reset_postdata(); } ?>
+              </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+      
+  <?php include 'includes/section-footer.php';?>
+
+<?php get_footer(); ?>
