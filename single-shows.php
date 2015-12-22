2f08b1f015d8129e9acbd3296083279fe0426ddc
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-22 12:31:31 +0100

    Update single-shows.php

--- single-shows.php
+++ single-shows.php
@@ -15,7 +15,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
-					<img src="<?php the_field(cover_image); ?>">
+					<img src="<?php the_field(cover_image); ?>" title="<?php the_title(); ?>" class="img-responsive">
 				</div>
 				<div class="col-md-8">
 					<?php the_content(); ?>
