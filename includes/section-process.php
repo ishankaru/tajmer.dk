287718fb53d31656154b05cc24642fb1da397ba0
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-22 18:45:30 +0100

    Update section-process.php

--- includes/section-process.php
+++ includes/section-process.php
@@ -17,7 +17,13 @@
 					<p class="h2">Referencer</p>
 					<div class="row">
 						<div class="col-md-12">
-							
+							<div class="gallery js-flickity">
+							  <div class="gallery-cell">1</div>
+							  <div class="gallery-cell">2</div>
+							  <div class="gallery-cell">3</div>
+							  <div class="gallery-cell">4</div>
+							  <div class="gallery-cell">5</div>
+							</div>	
 						</div>
 					</div>
 				</div>
