3a80b8c990e58e3af52f3498b20043f3aee42da2
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-26 18:45:50 +0100

    Update section-header.php

--- includes/section-header.php
+++ includes/section-header.php
@@ -25,7 +25,7 @@
 	</ul> 
 </header>
 
-<section class="module module-subnav hidden-xs hidden-sm">
+<section class="module module-subnav">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 col-lg-5">
