b9957508bcd89d0a543c0b5e54c1746c96544156
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-27 10:54:06 +0100

    Update section-calendar-single.php

--- includes/section-calendar-single.php
+++ includes/section-calendar-single.php
@@ -53,7 +53,7 @@
 							</td>
 							<td>
 								<?php if(get_sub_field('status') == "sold_out") { ?>
-    									<span class="btn btn-block btn-success" disabled><?php the_sub_field('button_text'); ?></span>
+    									<span class="btn btn-block btn-success" disabled="disabled"><?php the_sub_field('button_text'); ?></span>
     								<?php } else { ?>
     									<a href="<?php the_sub_field('url'); ?>" title="<?php the_sub_field('title'); ?>" class="btn btn-block btn-success"><?php the_sub_field('button_text'); ?></a>
     								<?php } ?>	
