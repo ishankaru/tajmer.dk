ab75d54bf426e2af8d8256a98e8cedd1a31c2ff6
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-28 18:15:03 +0100

    Update single-team.php

--- single-team.php
+++ single-team.php
@@ -10,24 +10,6 @@
  get_header(); ?>
 
  <?php include 'includes/section-header.php';?>
- 
-	<section class="module module-standard">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<img src="<?php the_field('image'); ?>" class="img-responsive img-circle">
-					<h3><?php the_field('name'); ?></h3>
-					<p>
-						<?php the_field('title'); ?><br>
-						<a href="mailto:<?php the_field('mail'); ?>"><?php the_field('mail'); ?></a><br>
-						<a href="tel:<?php the_field('phone'); ?>">+45 <?php the_field('phone'); ?></a><br>
-						<?php edit_post_link('Rediger', '', ''); ?>
-					</p>
-				</div>
-			</div>
-		</div>
-	</section>
-
  <?php include 'includes/section-footer.php'; ?>
 
 <?php get_footer(); ?>
