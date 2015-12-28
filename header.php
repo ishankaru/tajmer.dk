88f08d40501123b4dcd9436b60471e8720945a8c
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-28 16:57:49 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]><html lang="da" dir="ltr" class="ie6 no-js"><![endif]-->
-<!--[if IE 7 ]><html lang="da" dir="ltr" class="ie7 no-js"><![endif]-->
-<!--[if IE 8 ]><html lang="da" dir="ltr" class="ie8 no-js"><![endif]-->
-<!--[if IE 9 ]><html lang="da" dir="ltr" class="ie9 no-js"><![endif]-->
+<!--[if lt IE 7 ]><html lang="da" dir="ltr" class="ie6"><![endif]-->
+<!--[if IE 7 ]><html lang="da" dir="ltr" class="ie7"><![endif]-->
+<!--[if IE 8 ]><html lang="da" dir="ltr" class="ie8"><![endif]-->
+<!--[if IE 9 ]><html lang="da" dir="ltr" class="ie9"><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--><html lang="da" dir="ltr"><!--<![endif]-->
 <head>
 
