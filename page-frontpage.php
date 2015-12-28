e80cded1dc42fefbc52a8cb9e386d06632de118f
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-28 16:33:10 +0100

    Update page-frontpage.php

--- page-frontpage.php
+++ page-frontpage.php
@@ -96,7 +96,7 @@
 						?>
 
 						<?php if( $posts ) { foreach( $posts as $post ) { setup_postdata( $post ); ?> 
-							<div class="col-xs-4">
+							<div class="col-xs-4 pull-right">
 								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php the_field('cover_image'); ?>" alt="#" class="img-responsive"></a>
 								<a href="<?php the_permalink(); ?>">Se Tourplan</a>
 							</div>
