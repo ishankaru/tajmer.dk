3b24108a3fd77f10f447268578c3e98584f42f9e
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-29 18:20:18 +0100

    Update section-process.php

--- includes/section-process.php
+++ includes/section-process.php
@@ -13,26 +13,7 @@
 <section class="module module-process">
 		<div class="container">
 			<div class="row">
-				<div class="hidden-sm col-md-5">
-					<p class="h2"><?php the_field('client_title', options); ?></p>
-					<hr>
-					<div class="row">
-						<div class="col-md-12">
-							<div class="gallery clients js-flickity" data-flickity-options='{ "prevNextButtons": true, "pageDots": false, "lazyLoad": true, "wrapAround": true }'>
-								<?php while ( have_rows('clients_list', options) ) : the_row(); ?>
-							  		<div class="gallery-cell">
-									  	<img data-flickity-lazyload="<?php the_sub_field('logo'); ?>">
-										<span class="clientlogo"><?php the_sub_field('client_name'); ?></span>
-										<?php if( get_sub_field('testimonial') ): ?>
-											<span class="testimonial"><?php the_sub_field('testimonial'); ?></span>
-										<?php endif; ?>
-									  </div>
-								<?php endwhile; ?>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-offset-1 col-md-6">
+				<div class="col-md-12">
 					<p class="h2"><?php the_field('process_title', options); ?></p>
 					<hr>
 					<ul class="fa-ul">
