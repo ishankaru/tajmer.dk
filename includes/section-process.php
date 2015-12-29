8e52a01e6fa23798eb75ff7342ed8bfdc0dd4aaf
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-29 18:25:16 +0100

    Update section-process.php

--- includes/section-process.php
+++ includes/section-process.php
@@ -16,24 +16,24 @@
 				<div class="col-md-12">
 					<p class="h2"><?php the_field('process_title', options); ?></p>
 					<hr>
-					<ul class="fa-ul">
-						<li>
+					<div class="row">
+						<div class="col-md-3">
 							<i class="fa-li fa fa-envelope-o"></i>
 							<span><?php the_field('process_one', options); ?></span>
-						</li>
-						<li>
+						</div>
+						<div class="col-md-3">
 							<i class="fa-li fa fa-comments-o"></i>
 							<span><?php the_field('process_two', options); ?></span>
-						</li>
-						<li>
+						</div>
+						<div class="col-md-3">
 							<i class="fa-li fa fa-file-text-o"></i>
 							<span><?php the_field('process_three', options); ?></span>
-						</li>
-						<li>
+						</div>
+						<div class="col-md-3">
 							<i class="fa-li fa fa-thumbs-o-up"></i>
 							<span><?php the_field('process_four', options); ?></span>
-						</li>
-					</ul> 
+						</div>
+					</div>	
 				</div>													
 			</div>
 		</div>
