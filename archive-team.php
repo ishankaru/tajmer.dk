ab83c6ffd8fa16a109c228a49ad33f93710bbd86
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-29 17:54:44 +0100

    Update archive-team.php

--- archive-team.php
+++ archive-team.php
@@ -10,17 +10,6 @@
  get_header(); ?>
 
  <?php include 'includes/section-header.php';?>
- 
-	<section class="module module-standard">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<h1>Under udvikling</h1>
-				</div>
-			</div>
-		</div>
-	</section>
-
  <?php include 'includes/section-footer.php'; ?>
 
 <?php get_footer(); ?>
