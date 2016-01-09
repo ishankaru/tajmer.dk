9e0a1e7f20d6e55580cfb26a93c9218d587ab898
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2016-01-09 21:57:10 +0500

    Update page-calendar.php

--- page-calendar.php
+++ page-calendar.php
@@ -61,11 +61,11 @@
 							<?php foreach($calendar as $k => $v): ?>
 		<div class="row row-calendar <?php echo $v[5] ?>">
 			
-			<div class="col-xs-2">
-				<img src="<?php echo $v[6] ?>" alt="<?php echo $v[0] ?>">
+			<div class="col-xs-3 col-sm-1">
+				<img src="<?php echo $v[6] ?>" alt="<?php echo $v[0] ?>" class="img-responsive">
 			</div>
 
-			<div class="col-xs-10">
+			<div class="col-xs-9 col-sm-8">
 				<div class="row">
 					<div class="col-xs-12">
 						<?php 
@@ -90,7 +90,7 @@
 				</div>
 			</div>
 			
-			<div class="col-xs-12">
+			<div class="col-xs-12 col-sm-3">
 				<?php if ($v[5] == "sold_out") { ?>
 					<span class="btn btn-block btn-success" disabled><?php  echo $v[4]; ?></a>
 				<?php } else { ?>
